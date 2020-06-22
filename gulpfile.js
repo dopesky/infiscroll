24d57c204e9dd1996a2b4c6470176be196040bea
Dopesky <oboke69@gmail.com>
2020-06-22 19:05:40 +0300

    Added common functions for compilations of dev and prod states. Removed polyfills from this file.

--- gulpfile.js
+++ gulpfile.js
@@ -4,36 +4,39 @@ const browserSync = require('browser-sync').create(); // browser hot reload
 const sass = require('gulp-sass'); // sass compiler and minifier
 sass.compiler = require('node-sass'); // set the compiler for gulp-sass explicitly as advised in docs
 
-const browserify = require('browserify'); // convert my commonjs syntax to browser readable
+const browserify = require('browserify'); // convert my module syntax to browser readable
 const babelify = require('babelify'); // Use babel to transform my js to es2015
 const source = require('vinyl-source-stream'); // source the bundle after browserify is done working on it
 const buffer = require('vinyl-buffer'); // convert the stream from source-stream to a buffer we can do minification on
 const jsMinify = require('gulp-terser'); // js minifier
 
-task('compileJS', () => {
+const jsCompiler = () => {
     return browserify({
-        entries: ['./node_modules/@babel/polyfill/dist/polyfill.js', './src/js/infiscroll.js'],
+        entries: ['./src/js/infiscroll.js'],
         transform: [babelify]
-    }).bundle().pipe(source('infiscroll.js')).pipe(buffer()).pipe(jsMinify()).pipe(dest('./dist/js'))
-    .pipe(browserSync.reload({stream: true}));
+    }).bundle().pipe(source('infiscroll.js')).pipe(buffer()).pipe(jsMinify()).pipe(dest('./dist/js'));
+};
+
+sassCompiler = () => {
+    return src('./src/sass/infiscroll.scss').pipe(sass({outputStyle: 'compressed'})
+        .on('error', sass.logError)).pipe(dest('./dist/css'));
+};
+
+task('compileJS', () => {
+    return jsCompiler().pipe(browserSync.reload({stream: true}));
 });
 
 task('compileSass', () => {
-    return src('./src/sass/infiscroll.scss').pipe(sass({outputStyle: 'compressed'})
-        .on('error', sass.logError)).pipe(dest('./dist/css')).pipe(browserSync.reload({stream: true}));
+    return sassCompiler().pipe(browserSync.reload({stream: true}));
 });
 
 task('default', () => {
     browserSync.init({proxy: 'localhost/Html/InfiScroll/docs/', baseDir: './', open: false});
-    watch('./src/sass/infiscroll.scss',{ ignoreInitial: false }, parallel('compileSass'));
-    watch('./src/js/infiscroll.js', { ignoreInitial: false }, parallel('compileJS'));
+    watch('./src/sass/infiscroll.scss', {ignoreInitial: false}, parallel('compileSass'));
+    watch('./src/js/infiscroll.js', {ignoreInitial: false}, parallel('compileJS'));
     watch('./docs/**/*.php').on('change', browserSync.reload);
 });
 
 task('deploy', () => {
-    return browserify({
-        entries: ['./node_modules/@babel/polyfill/dist/polyfill.js', './src/js/infiscroll.js'],
-        transform: [babelify]
-    }).bundle().pipe(source('infiscroll.js')).pipe(buffer()).pipe(jsMinify()).pipe(dest('./dist/js'))
-    && src('./src/sass/infiscroll.scss').pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError)).pipe(dest('./dist/css'));
+    return jsCompiler() && sassCompiler();
 });
